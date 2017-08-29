e933447aa5d2290930817f9d115735b0eb77e60a
DESKTOP-6NPNQRF\ljtys <ljtyson@gmail.com>
2017-08-28 20:28:16 -0400

    initial commit

---
+++ HelloWorldApplication/src/HelloWorld.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2009 Sun Microsystems, Inc.
+ * All rights reserved.  You may not modify, use,
+ * reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://developer.sun.com/berkeley_license.html
+ */
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+
+/**
+ * REST Web Service
+ *
+ * @author Nathan
+ */
+@Path("helloworld")
+public class HelloWorld {
+    @Context
+    private UriInfo context;
+
+    /** Creates a new instance of HelloWorld */
+    public HelloWorld() {
+    }
+
+    /**
+     * Retrieves representation of an instance of helloWorld.HelloWorld
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html><body><h1>Hello, World!!</h1></body></html>";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HelloWorld
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("text/html")
+    public void putHtml(String content) {
+    }
+}
